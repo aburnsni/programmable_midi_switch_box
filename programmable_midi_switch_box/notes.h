df0d80165bd050e44f40a82e5e9d4502d63fc64d
Andrew Burns <aburnsni@gmail.com>
2018-09-12 12:45:07 +0100

    Moved note names to PROGMEM and moved to includes.

---
+++ programmable_midi_switch_box/notes.h
@@ -0,0 +1,260 @@
+const char note_0[] PROGMEM = "C-2";
+const char note_1[] PROGMEM = "C#-2";
+const char note_2[] PROGMEM = "D-2";
+const char note_3[] PROGMEM = "Eb-2";
+const char note_4[] PROGMEM = "E-2";
+const char note_5[] PROGMEM = "F-2";
+const char note_6[] PROGMEM = "F#-2";
+const char note_7[] PROGMEM = "G-2";
+const char note_8[] PROGMEM = "G#-2";
+const char note_9[] PROGMEM = "A-2";
+const char note_10[] PROGMEM = "Bb-2";
+const char note_11[] PROGMEM = "B-2";
+const char note_12[] PROGMEM = "C-1";
+const char note_13[] PROGMEM = "C#-1";
+const char note_14[] PROGMEM = "D-1";
+const char note_15[] PROGMEM = "Eb-1";
+const char note_16[] PROGMEM = "E-1";
+const char note_17[] PROGMEM = "F-1";
+const char note_18[] PROGMEM = "F#-1";
+const char note_19[] PROGMEM = "G-1";
+const char note_20[] PROGMEM = "G#-1";
+const char note_21[] PROGMEM = "A-1";
+const char note_22[] PROGMEM = "Bb-1";
+const char note_23[] PROGMEM = "B-1";
+const char note_24[] PROGMEM = "C0";
+const char note_25[] PROGMEM = "C#0";
+const char note_26[] PROGMEM = "D0";
+const char note_27[] PROGMEM = "Eb0";
+const char note_28[] PROGMEM = "E0";
+const char note_29[] PROGMEM = "F0";
+const char note_30[] PROGMEM = "F#0";
+const char note_31[] PROGMEM = "G0";
+const char note_32[] PROGMEM = "G#0";
+const char note_33[] PROGMEM = "A0";
+const char note_34[] PROGMEM = "Bb0";
+const char note_35[] PROGMEM = "B0";
+const char note_36[] PROGMEM = "C1";
+const char note_37[] PROGMEM = "C#1";
+const char note_38[] PROGMEM = "D1";
+const char note_39[] PROGMEM = "Eb1";
+const char note_40[] PROGMEM = "E1";
+const char note_41[] PROGMEM = "F1";
+const char note_42[] PROGMEM = "F#1";
+const char note_43[] PROGMEM = "G1";
+const char note_44[] PROGMEM = "G#1";
+const char note_45[] PROGMEM = "A1";
+const char note_46[] PROGMEM = "Bb1";
+const char note_47[] PROGMEM = "B1";
+const char note_48[] PROGMEM = "C2";
+const char note_49[] PROGMEM = "C#2";
+const char note_50[] PROGMEM = "D2";
+const char note_51[] PROGMEM = "Eb2";
+const char note_52[] PROGMEM = "E2";
+const char note_53[] PROGMEM = "F2";
+const char note_54[] PROGMEM = "F#2";
+const char note_55[] PROGMEM = "G2";
+const char note_56[] PROGMEM = "G#2";
+const char note_57[] PROGMEM = "A2";
+const char note_58[] PROGMEM = "Bb2";
+const char note_59[] PROGMEM = "B2";
+const char note_60[] PROGMEM = "C3";
+const char note_61[] PROGMEM = "C#3";
+const char note_62[] PROGMEM = "D3";
+const char note_63[] PROGMEM = "Eb3";
+const char note_64[] PROGMEM = "E3";
+const char note_65[] PROGMEM = "F3";
+const char note_66[] PROGMEM = "F#3";
+const char note_67[] PROGMEM = "G3";
+const char note_68[] PROGMEM = "G#3";
+const char note_69[] PROGMEM = "A3";
+const char note_70[] PROGMEM = "Bb3";
+const char note_71[] PROGMEM = "B3";
+const char note_72[] PROGMEM = "C4";
+const char note_73[] PROGMEM = "C#4";
+const char note_74[] PROGMEM = "D4";
+const char note_75[] PROGMEM = "Eb4";
+const char note_76[] PROGMEM = "E4";
+const char note_77[] PROGMEM = "F4";
+const char note_78[] PROGMEM = "F#4";
+const char note_79[] PROGMEM = "G4";
+const char note_80[] PROGMEM = "G#4";
+const char note_81[] PROGMEM = "A4";
+const char note_82[] PROGMEM = "Bb4";
+const char note_83[] PROGMEM = "B4";
+const char note_84[] PROGMEM = "C5";
+const char note_85[] PROGMEM = "C#5";
+const char note_86[] PROGMEM = "D5";
+const char note_87[] PROGMEM = "Eb5";
+const char note_88[] PROGMEM = "E5";
+const char note_89[] PROGMEM = "F5";
+const char note_90[] PROGMEM = "F#5";
+const char note_91[] PROGMEM = "G5";
+const char note_92[] PROGMEM = "G#5";
+const char note_93[] PROGMEM = "A5";
+const char note_94[] PROGMEM = "Bb5";
+const char note_95[] PROGMEM = "B5";
+const char note_96[] PROGMEM = "C6";
+const char note_97[] PROGMEM = "C#6";
+const char note_98[] PROGMEM = "D6";
+const char note_99[] PROGMEM = "Eb6";
+const char note_100[] PROGMEM = "E6";
+const char note_101[] PROGMEM = "F6";
+const char note_102[] PROGMEM = "F#6";
+const char note_103[] PROGMEM = "G6";
+const char note_104[] PROGMEM = "G#6";
+const char note_105[] PROGMEM = "A6";
+const char note_106[] PROGMEM = "Bb6";
+const char note_107[] PROGMEM = "B6";
+const char note_108[] PROGMEM = "C7";
+const char note_109[] PROGMEM = "C#7";
+const char note_110[] PROGMEM = "D7";
+const char note_111[] PROGMEM = "Eb7";
+const char note_112[] PROGMEM = "E7";
+const char note_113[] PROGMEM = "F7";
+const char note_114[] PROGMEM = "F#7";
+const char note_115[] PROGMEM = "G7";
+const char note_116[] PROGMEM = "G#7";
+const char note_117[] PROGMEM = "A7";
+const char note_118[] PROGMEM = "Bb7";
+const char note_119[] PROGMEM = "B7";
+const char note_120[] PROGMEM = "C8";
+const char note_121[] PROGMEM = "C#8";
+const char note_122[] PROGMEM = "D8";
+const char note_123[] PROGMEM = "Eb8";
+const char note_124[] PROGMEM = "E8";
+const char note_125[] PROGMEM = "F8";
+const char note_126[] PROGMEM = "F#8";
+const char note_127[] PROGMEM = "G8";
+
+
+const char * const note_table[] PROGMEM = {   
+note_0,
+note_1,
+note_2,
+note_3,
+note_4,
+note_5,
+note_6,
+note_7,
+note_8,
+note_9,
+note_10,
+note_11,
+note_12,
+note_13,
+note_14,
+note_15,
+note_16,
+note_17,
+note_18,
+note_19,
+note_20,
+note_21,
+note_22,
+note_23,
+note_24,
+note_25,
+note_26,
+note_27,
+note_28,
+note_29,
+note_30,
+note_31,
+note_32,
+note_33,
+note_34,
+note_35,
+note_36,
+note_37,
+note_38,
+note_39,
+note_40,
+note_41,
+note_42,
+note_43,
+note_44,
+note_45,
+note_46,
+note_47,
+note_48,
+note_49,
+note_50,
+note_51,
+note_52,
+note_53,
+note_54,
+note_55,
+note_56,
+note_57,
+note_58,
+note_59,
+note_60,
+note_61,
+note_62,
+note_63,
+note_64,
+note_65,
+note_66,
+note_67,
+note_68,
+note_69,
+note_70,
+note_71,
+note_72,
+note_73,
+note_74,
+note_75,
+note_76,
+note_77,
+note_78,
+note_79,
+note_80,
+note_81,
+note_82,
+note_83,
+note_84,
+note_85,
+note_86,
+note_87,
+note_88,
+note_89,
+note_90,
+note_91,
+note_92,
+note_93,
+note_94,
+note_95,
+note_96,
+note_97,
+note_98,
+note_99,
+note_100,
+note_101,
+note_102,
+note_103,
+note_104,
+note_105,
+note_106,
+note_107,
+note_108,
+note_109,
+note_110,
+note_111,
+note_112,
+note_113,
+note_114,
+note_115,
+note_116,
+note_117,
+note_118,
+note_119,
+note_120,
+note_121,
+note_122,
+note_123,
+note_124,
+note_125,
+note_126,
+note_127
+};
